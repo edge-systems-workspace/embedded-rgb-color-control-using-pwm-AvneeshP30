de81eb0f1fdb4f4f7c4464fa1018990ac3c00ded
AvneeshP30 <avneesh.pandey_cs.aiml24@gla.ac.in>
2026-02-20 11:45:10 +0530

    define RGB pin constants in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,10 @@
  * and analog PWM brightness control.
  */
 
- // TODO 1:
- // Define RED pin (Use 9)
+int redPin = 9;
+int greenPin = 10;
+int bluePin = 11;
 
- // TODO 2:
- // Define GREEN pin (Use 10)
-
- // TODO 3:
- // Define BLUE pin (Use 11)
 
 void setup() {
 
